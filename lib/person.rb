398c52ffb70dcef97860ae74192b90f9f084d2db
Tori Sigler <mssigler@gmail.com>
2019-06-05 13:14:43 -0400

    Did the person lab

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,142 @@
 # your code goes here
+require 'pry'
+
+class Person
+
+	attr_reader :name, :happiness, :hygiene
+	attr_accessor :bank_account
+
+
+	def initialize(name, bank_account = 25, happiness = 8, hygiene =8)
+		@name = name
+		@bank_account = bank_account
+		
+		@happiness = happiness	#call for the writer method
+
+		@hygiene = hygiene
+
+	end 						#instantiation end
+
+
+	def happiness=(happiness)	#writer method
+		if happiness > 10
+			@happiness = 10
+		elsif happiness < 0
+			@happiness = 0
+		else
+			@happiness = happiness		
+		end
+	end
+
+
+	def hygiene=(hygiene)
+		if hygiene > 10
+			@hygiene = 10
+		elsif hygiene < 0
+			@hygiene = 0
+		else
+			@hygiene = hygiene		
+		end
+	end
+
+
+	def happy?
+		if @happiness > 7
+			return true
+		else
+			return false
+		end
+	end
+
+
+	def clean?
+		if @hygiene > 7
+			return true
+		else
+			return false
+		end
+	end
+
+
+	def get_paid(salary_amount)
+		@bank_account += salary_amount
+		return "all about the benjamins"
+	end
+
+
+	def take_bath
+		print "@hygiene is: #{@hygiene}"
+
+		self.hygiene+= 4 		#call writer method outside instantiation
+			return '♪ Rub-a-dub just relaxing in the tub ♫'
+	end 
+
+
+	def work_out
+		#@happiness = happiness + 2
+		self.happiness+=2
+		self.hygiene-=3
+		return '♪ another one bites the dust ♫'
+
+	end
+
+	def call_friend(friend)
+		self.happiness += 3
+		friend.happiness += 3
+
+		return "Hi #{friend.name}! It's #{self.name}. How are you?"
+	end
+
+
+	def start_conversation(friend, topic)
+		if topic == "politics"
+			self.happiness -=2
+			friend.happiness -=2
+			return "blah blah partisan blah lobbyist"
+		elsif topic == "weather"
+			self.happiness +=1
+			friend.happiness +=1
+			return "blah blah sun blah rain"
+		else
+			return 'blah blah blah blah blah'
+		end
+			
+	end
+
+
+
+	# def happy_points_check(value_in)
+	# 	if ((@happiness + value_in) < 10) && ((@happiness + value_in) > 0) 
+	# 		@happiness += value_in
+	# 	elsif happiness < 0
+	# 		@happiness = 0
+	# 	else 
+	# 		@happiness = 10
+	# 	end
+	# end	
+
+	# def hygiene_points_check(value_in)
+	# 	if ((@hygiene + value_in) < 10) && ((@hygiene + value_in) > 0) 
+	# 		@hygiene += value_in
+	# 	elsif hygiene < 0
+	# 		@hygiene = 0
+	# 	else 
+	# 		@hygiene = 10
+	# 	end
+	# end	
+
+
+
+
+end
+
+
+
+		# if hygiene < 10 && hygiene > 0 
+		# 	@hygiene = hygiene
+		# elsif hygiene < 0
+		# 	@hygiene = 0
+		# else 
+		# 	@hygiene = 10
+		# end
+
